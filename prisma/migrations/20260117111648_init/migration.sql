0c4051fa205e1805ee389f3ea4b48bd548ce01c6
Md.Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2026-01-17 17:17:46 +0600

    user profile migrate

---
+++ prisma/migrations/20260117111648_init/migration.sql
@@ -0,0 +1,52 @@
+-- CreateEnum
+CREATE TYPE "AuthenticationMethod" AS ENUM ('Email', 'Phone', 'Google', 'Facebook', 'Apple');
+
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "username" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "phoneNumber" TEXT,
+    "passwordHash" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "isVerified" BOOLEAN NOT NULL DEFAULT false,
+    "authenticationMethod" "AuthenticationMethod" NOT NULL,
+    "lastLogin" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Profile" (
+    "id" SERIAL NOT NULL,
+    "userId" INTEGER NOT NULL,
+    "location" TEXT NOT NULL,
+    "age" INTEGER NOT NULL,
+    "skillLevel" TEXT NOT NULL,
+    "playingFrequency" TEXT NOT NULL,
+    "preferences" JSONB NOT NULL,
+    "profileImage" TEXT,
+    "bio" TEXT,
+    "lastLogin" TIMESTAMP(3),
+    "visibility" BOOLEAN NOT NULL DEFAULT true,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+
+    CONSTRAINT "Profile_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_username_key" ON "User"("username");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "User_phoneNumber_key" ON "User"("phoneNumber");
+
+-- CreateIndex
+CREATE UNIQUE INDEX "Profile_userId_key" ON "Profile"("userId");
+
+-- AddForeignKey
+ALTER TABLE "Profile" ADD CONSTRAINT "Profile_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
