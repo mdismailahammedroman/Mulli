991c4e30417711794c8ae48ec768c4df9e7f5a2c
Md.Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2026-01-19 11:51:25 +0600

    CatchAsync

---
+++ src/utills/CatchAsync.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+
+export const CatchAsync = (
+  fn: (req: Request, res: Response, next: NextFunction) => Promise<void>,
+) => {
+  return (req: Request, res: Response, next: NextFunction) => {
+    try {
+      fn(req, res, next);
+    } catch (error) {
+      next(error);
+    }
+  };
+};
