fc4a8f12d8353013adc08541e6ce5f17afb6ccd1
Md.Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2026-01-23 18:03:26 +0600

    httstatuscode add

--- src/app/module/user/user.controller.ts
+++ src/app/module/user/user.controller.ts
@@ -1,6 +1,8 @@
 import { NextFunction, Request, Response } from "express";
 import { UserService } from "./user.service";
 import { CatchAsync } from "../../../utils/CatchAsync";
+import { SendResponse } from "../../../utils/SendResponse";
+import { StatusCodes } from "http-status-codes";
 
 const createUserHandler = CatchAsync(
   async (req: Request, res: Response, next: NextFunction) => {
@@ -8,8 +10,10 @@ const createUserHandler = CatchAsync(
 
     const result = await UserService.createUser(payload);
 
-    res.status(201).json({
+    SendResponse(res, {
       success: true,
+      message: "user created successfully",
+      StatusCode: StatusCodes.OK,
       data: result,
     });
   },
