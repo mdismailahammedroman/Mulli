3b915697a569e3f75d1d27a63bc15e75245a09c6
Md.Ismail Ahammed Roman <ismailahmmedroman@gmail.com>
2026-01-23 17:51:31 +0600

    server update

--- src/utils/SeedSuperAdmin.ts
+++ src/utils/SeedSuperAdmin.ts
@@ -1,40 +1,36 @@
 import bcrypt from "bcryptjs";
-import { NextFunction, Request, Response } from "express";
-import { CatchAsync } from "./CatchAsync";
 import { prisma } from "../app/lib/prisma";
 import { EnvVars } from "../app/config/EnvVars";
 import { AuthProviderType, UserRole } from "@prisma/client";
 
-export const CreateSuperAdmin = CatchAsync(
-  async (req: Request, res: Response, next: NextFunction) => {
-    const existingAdmin = await prisma.user.findUnique({
-      where: { email: EnvVars.SUPER_ADMIN_EMAIL },
-    });
+export const seedSuperAdmin = async () => {
+  const existingAdmin = await prisma.user.findUnique({
+    where: { email: EnvVars.SUPER_ADMIN_EMAIL },
+  });
 
-    if (existingAdmin) {
-      console.log("Super Admin Already Exists!");
-      return;
-    }
+  if (existingAdmin) {
+    console.log("Super Admin Already Exists!");
+    return;
+  }
 
-    console.log("Trying to create Super Admin...");
+  console.log("Creating Super Admin...");
 
-    const hashedPassword = await bcrypt.hash(
-      EnvVars.SUPER_ADMIN_PASS,
-      EnvVars.BCRYPT_SALT_ROUND,
-    );
+  const hashedPassword = await bcrypt.hash(
+    EnvVars.SUPER_ADMIN_PASS,
+    Number(EnvVars.BCRYPT_SALT_ROUND) || 12,
+  );
 
-    const superAdmin = await prisma.user.create({
-      data: {
-        name: "Super Admin",
-        email: EnvVars.SUPER_ADMIN_EMAIL,
-        Role: UserRole.SUPER_ADMIN,
-        isVerified: true,
-        password: hashedPassword,
-        authProvider: AuthProviderType.CREDENTIAL,
-        providerID: EnvVars.SUPER_ADMIN_EMAIL,
-      },
-    });
+  const superAdmin = await prisma.user.create({
+    data: {
+      name: "Super Admin",
+      email: EnvVars.SUPER_ADMIN_EMAIL,
+      Role: UserRole.SUPER_ADMIN,
+      isVerified: true,
+      password: hashedPassword,
+      authProvider: AuthProviderType.CREDENTIAL,
+      providerID: EnvVars.SUPER_ADMIN_EMAIL, // required by Prisma
+    },
+  });
 
-    console.log("Super Admin created successfully:", superAdmin.email);
-  },
-);
+  console.log("Super Admin created successfully:", superAdmin.email);
+};
